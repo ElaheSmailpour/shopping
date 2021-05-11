d4f6f70ee3ff03187c694cbdac3933f805be2c1c
Elahesmailpour <eli_es87@yahoo.com>
2021-05-11 10:14:34 +0200

    signups

--- app/model/user.js
+++ app/model/user.js
@@ -6,8 +6,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     gender: {
         type: String,
-        enum: ["male", "female"],
-        default: "male"
+        enum: ["Male", "Female","Other"],
+        default: "Male"
 
     }
 })
